f32c420c1dada8a33f96694c2b44486393a61700
ddelsere <delseredelfina@gmail.com>
2024-11-01 16:07:56 -0300

    Fix en ValidateUrlScreen para que funcione bien el routeo

--- App/crypty/app/(tabs)/index.tsx
+++ App/crypty/app/(tabs)/index.tsx
@@ -3,11 +3,13 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SelectImageScreen from '../SelectImageScreen';
 import ValidateImageScreen from '../ValidateImageScreen';
+import ValidateUrlScreen from '../ValidateUrlScreen';
 import ResultScreen from '../ResultScreen';
 
 type RootStackParamList = {
   SelectImage: undefined;
   ValidateImage: { imageUri: string };
+  ValidateUrl: { url: string };
   ResultScreen: { imageUri: string, validationResult: number };
 };
 
@@ -15,11 +17,11 @@ const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    // The only NavigationContainer in the app should be here
     <NavigationContainer independent={true}>
-      <Stack.Navigator initialRouteName="SelectImage" >
-        <Stack.Screen name="SelectImage" component={SelectImageScreen} options={{ title: 'Seleccionar imagen' }}  />
+      <Stack.Navigator initialRouteName="SelectImage">
+        <Stack.Screen name="SelectImage" component={SelectImageScreen} options={{ title: 'Seleccionar imagen' }} />
         <Stack.Screen name="ValidateImage" component={ValidateImageScreen} options={{ title: 'Validar imagen' }} />
+        <Stack.Screen name="ValidateUrl" component={ValidateUrlScreen} options={{ title: 'Validar URL' }} />
         <Stack.Screen name="ResultScreen" component={ResultScreen} options={{ title: 'Resultado' }} />
       </Stack.Navigator>
     </NavigationContainer>
